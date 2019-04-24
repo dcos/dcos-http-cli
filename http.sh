f8aefaf1fa406db0d6560edde6a50916c6a68dd7
Bilal Amarni <bilal.amarni@gmail.com>
2019-04-24 15:49:38 +0200

    Support completion for --request after a space

--- http.sh
+++ http.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 _dcos_http() {
-    local i command
-
-    if ! __dcos_default_command_parse; then
-        return
-    fi
-
     local flags=(
     "--data"
     "--request="
@@ -23,22 +17,29 @@ _dcos_http() {
     "CONNECT"
     )
 
-    if [ -z "$command" ]; then
-        case "$cur" in
-            --request=*)
-                # Get HTTP methods completions using compgen, we strip "--request=" from
-                # the current word to only keep the current option value, if any.
-                local compreply=($(compgen -W "${methods[*]}" -- "${cur#*=}"))
+    if [ $prev == "--request" ]; then
+        local compreply=($(compgen -W "${methods[*]}" -- "${cur}"))
 
-                # Add spaces after each array element to circumvent the "nospace" option.
-                COMPREPLY=( "${compreply[@]/%/ }" )
-                ;;
-            --*)
-                __dcos_handle_compreply "${flags[@]}"
-                ;;
-            *)
-                ;;
-        esac
+        # Add spaces after each array element to circumvent the "nospace" option.
+        COMPREPLY=( "${compreply[@]/%/ }" )
         return
     fi
+
+    case "$cur" in
+        --request=*)
+            # Get HTTP methods completions using compgen, we strip "--request=" from
+            # the current word to only keep the current option value, if any.
+            local compreply=($(compgen -W "${methods[*]}" -- "${cur#*=}"))
+
+            # Add spaces after each array element to circumvent the "nospace" option.
+            COMPREPLY=( "${compreply[@]/%/ }" )
+            ;;
+        --*)
+            __dcos_handle_compreply "${flags[@]}"
+            ;;
+        *)
+            ;;
+    esac
+    return
+
 }
